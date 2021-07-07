89d5fb93d095dbaa19aadffaafd917e3cc9fcdf3
lea.arturi <lea.arturi@gmail.com>
2021-07-07 18:37:11 -0300

    Basic Modal Component

--- components/Header/Menu/Menu.js
+++ components/Header/Menu/Menu.js
@@ -1,9 +1,16 @@
 /* eslint-disable @next/next/link-passhref */
 
+import { useState } from "react";
 import { Container, Menu, Grid, Icon, Label } from "semantic-ui-react";
 import Link from "next/link";
 
+import BasicModal from "../../Modal/BasicModal/BasicModal";
+
 const MenuHeader = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const onShowModal = () => setShowModal(true);
+
   return (
     <div className="menu">
       <Container>
@@ -12,10 +19,18 @@ const MenuHeader = () => {
             <MenuPlatforms />
           </Grid.Column>
           <Grid.Column className="menu__right" width={10}>
-            <MenuOptionsUser />
+            <MenuOptionsUser onShowModal={onShowModal} />
           </Grid.Column>
         </Grid>
       </Container>
+      <BasicModal
+        show={showModal}
+        setShow={setShowModal}
+        title="Crear tu cuenta"
+        size="small"
+      >
+        <h3>Modal Content</h3>
+      </BasicModal>
     </div>
   );
 };
@@ -36,10 +51,11 @@ const MenuPlatforms = () => {
   );
 };
 
-const MenuOptionsUser = () => {
+const MenuOptionsUser = (props) => {
+  const { onShowModal } = props;
   return (
     <Menu>
-      <Menu.Item>
+      <Menu.Item onClick={onShowModal}>
         <Icon name="user outline" />
         Mi Cuenta
       </Menu.Item>
