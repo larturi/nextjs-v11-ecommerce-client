73f65a91415208022768a0c66764ed730c0f1be7
lea.arturi <lea.arturi@gmail.com>
2021-07-11 12:10:18 -0300

    Ajustes para que Formik valide el Select

--- components/Account/AddressForm/AddressForm.js
+++ components/Account/AddressForm/AddressForm.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Button, Dropdown } from 'semantic-ui-react';
+import { Form, Button } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import useAuth from '../../../hooks/useAuth';
 import { getProvinciasApi } from '../../../api/provincias';
@@ -10,6 +12,19 @@ const AddressForm = () => {
    ]);
 
    const { logout } = useAuth();
+
+   const formik = useFormik({
+      initialValues: initialValues(),
+      mapPropsToValues: () => ({ state: '' }),
+      validationSchema: Yup.object(validationSchema()),
+      onSubmit: (formData) => {
+         console.log(formData);
+      },
+   });
+
+   const handleChangeProvincia = (event, { value }) => {
+      formik.values.state = value;
+   };
 
    useEffect(() => {
       (async () => {
@@ -27,12 +42,15 @@ const AddressForm = () => {
    }, []);
 
    return (
-      <Form className='address-form'>
+      <Form className='address-form' onSubmit={formik.handleSubmit}>
          <Form.Input
             name='title'
             type='text'
             label='Titulo del domicilio'
             placeholder='Titulo del domicilio'
+            onChange={formik.handleChange}
+            value={formik.values.title}
+            error={formik.errors.title}
          />
 
          <Form.Group widths='equal'>
@@ -41,12 +59,18 @@ const AddressForm = () => {
                type='text'
                label='Nombre y Apellido'
                placeholder='Nombre y Apellido'
+               onChange={formik.handleChange}
+               value={formik.values.name}
+               error={formik.errors.name}
             />
             <Form.Input
                name='address'
                type='text'
                label='Dirección'
                placeholder='Dirección'
+               onChange={formik.handleChange}
+               value={formik.values.address}
+               error={formik.errors.address}
             />
          </Form.Group>
 
@@ -56,6 +80,9 @@ const AddressForm = () => {
                type='text'
                label='Ciudad'
                placeholder='Ciudad'
+               onChange={formik.handleChange}
+               value={formik.values.city}
+               error={formik.errors.city}
             />
             <Form.Dropdown
                name='state'
@@ -63,6 +90,8 @@ const AddressForm = () => {
                label='Provincia/Estado'
                selection
                options={provincias}
+               onChange={handleChangeProvincia}
+               error={formik.errors.state}
             />
          </Form.Group>
 
@@ -72,12 +101,18 @@ const AddressForm = () => {
                type='text'
                label='Código Postal'
                placeholder='Código Postal'
+               onChange={formik.handleChange}
+               value={formik.values.postalCode}
+               error={formik.errors.postalCode}
             />
             <Form.Input
                name='phone'
                type='text'
                label='Número de Teléfono'
                placeholder='Número de Teléfono'
+               onChange={formik.handleChange}
+               value={formik.values.phone}
+               error={formik.errors.phone}
             />
          </Form.Group>
 
@@ -91,3 +126,27 @@ const AddressForm = () => {
 };
 
 export default AddressForm;
+
+const initialValues = () => {
+   return {
+      title: '',
+      name: '',
+      address: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      phone: '',
+   };
+};
+
+const validationSchema = () => {
+   return {
+      title: Yup.string().required(true),
+      name: Yup.string().required(true),
+      address: Yup.string().required(true),
+      city: Yup.string().required(true),
+      state: Yup.string().required(true),
+      postalCode: Yup.string().required(true),
+      phone: Yup.string().required(true),
+   };
+};
